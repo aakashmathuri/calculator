f000ceed20798426c37e250c7c61e46babd881a0
Aakash <aakash1350@gmail.com>
2020-06-05 18:32:02 +0530

    RemovedNew

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,127 +1,132 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   var num1 = 0, num2 = 0, sum = 0;
 
   final TextEditingController t1 = new TextEditingController(text: "0");
   final TextEditingController t2 = new TextEditingController(text: "0");
 
-
-  void doAddition(){
+  void doAddition() {
     setState(() {
       num1 = int.parse(t1.text);
       num2 = int.parse(t2.text);
       sum = num1 + num2;
     });
   }
-  void doSub(){
+
+  void doSub() {
     setState(() {
       num1 = int.parse(t1.text);
       num2 = int.parse(t2.text);
       sum = num1 - num2;
     });
   }
-  void doMul(){
+
+  void doMul() {
     setState(() {
       num1 = int.parse(t1.text);
       num2 = int.parse(t2.text);
       sum = num1 * num2;
     });
   }
-  void doDiv(){
+
+  void doDiv() {
     setState(() {
       num1 = int.parse(t1.text);
       num2 = int.parse(t2.text);
       sum = num1 ~/ num2;
     });
   }
-  void doClear(){
+
+  void doClear() {
     setState(() {
       t1.text = "0";
       t2.text = "0";
     });
   }
-  @override 
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Calculator"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator"),
       ),
-      body: new Container(
-          padding: const EdgeInsets.all(40.0),
-        child: new Column(
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text(
+            Text(
               "Output : $sum",
-              style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.purple),
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple),
             ),
-            new TextField(
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: new InputDecoration(
-                hintText: "Enter number 1"),
+              decoration: InputDecoration(hintText: "Enter number 1"),
               controller: t1,
             ),
-            new TextField(
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: new InputDecoration(
-                hintText: "Enter number 2"),
-                controller: t2,
+              decoration: new InputDecoration(hintText: "Enter number 2"),
+              controller: t2,
             ),
-            new Padding(
+            Padding(
               padding: const EdgeInsets.only(top: 20.0),
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  new MaterialButton(
-                    child: new Text("+"),
-                    color: Colors.greenAccent,
-                    onPressed: doAddition,
-                    ),
-                    new MaterialButton(
-                    child: new Text("-"),
-                    color: Colors.greenAccent,
-                    onPressed: doSub,
-                    ),
-                   ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                MaterialButton(
+                  child: Text("+"),
+                  color: Colors.greenAccent,
+                  onPressed: doAddition,
                 ),
-                 new Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  new MaterialButton(
-                    child: new Text("*"),
-                    color: Colors.greenAccent,
-                    onPressed: doMul,
-                    ),
-                    new MaterialButton(
-                    child: new Text("/"),
-                    color: Colors.greenAccent,
-                    onPressed: doDiv,
-                    ),
-                   ],
+                MaterialButton(
+                  child: Text("-"),
+                  color: Colors.greenAccent,
+                  onPressed: doSub,
                 ),
-                new Padding(
+              ],
+            ),
+            Padding(
               padding: const EdgeInsets.only(top: 20.0),
-              ),
-                new Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new MaterialButton(
-                    child: new Text("Clear"),
-                    color: Colors.greenAccent,
-                    onPressed: doClear,
-                    ),
-                  ],
-                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                MaterialButton(
+                  child: Text("*"),
+                  color: Colors.greenAccent,
+                  onPressed: doMul,
+                ),
+                MaterialButton(
+                  child: Text("/"),
+                  color: Colors.greenAccent,
+                  onPressed: doDiv,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                MaterialButton(
+                  child: Text("Clear"),
+                  color: Colors.greenAccent,
+                  onPressed: doClear,
+                ),
+              ],
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return new MaterialApp(
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "Calculator App",
-      theme: new ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home: new HomePage(),
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
     );
   }
 }
